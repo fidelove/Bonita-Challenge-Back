536ed93fbfef959020b4eb5a452c2b0db58387f6
Fidel Besada Juarez <pontianus@gmail.com>
2022-05-15 12:30:49 +0200

    Added email sent when a new user is created
    Added email sent when a new user is created

--- src/main/java/com/bonitasoft/challenge/controller/UserController.java
+++ src/main/java/com/bonitasoft/challenge/controller/UserController.java
@@ -1,7 +1,21 @@
 package com.bonitasoft.challenge.controller;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -27,6 +41,49 @@ import com.google.common.collect.Lists;
 public class UserController extends AbstractController {
 
 	Logger logger = LogManager.getLogger(UserController.class);
+
+	/**
+	 * Send an email to the new created user
+	 * 
+	 * @param user Object containing the user information
+	 * @throws MessagingException
+	 * @throws AddressException
+	 * @throws IOException
+	 */
+	private void sendEmailNewUser(User user) {
+
+		try {
+			Properties props = new Properties();
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "587");
+
+			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("bonitasoft.challenge@gmail.com", "BoNiTaSoFtPaSsWoRd");
+				}
+			});
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress("bonitasoft.challenge@gmail.com", false));
+
+			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(user.getUserEmail()));
+			msg.setSubject("New account created");
+			msg.setContent("Your new account has been created successfully", "text/html");
+			msg.setSentDate(new Date());
+
+			MimeBodyPart messageBodyPart = new MimeBodyPart();
+			messageBodyPart.setContent("Your new account has been created successfully", "text/html");
+
+			Multipart multipart = new MimeMultipart();
+			multipart.addBodyPart(messageBodyPart);
+			msg.setContent(multipart);
+			Transport.send(msg);
+
+		} catch (MessagingException e) {
+			logger.error("The email couldn't be sent with this error: " + e.getMessage(), e);
+		}
+	}
 
 	@GetMapping(path = "/users")
 	public List<User> allUsers() {
@@ -69,6 +126,7 @@ public class UserController extends AbstractController {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The user already exists");
 
 		} else {
+			sendEmailNewUser(user);
 			return userRepo.save(user);
 		}
 	}
